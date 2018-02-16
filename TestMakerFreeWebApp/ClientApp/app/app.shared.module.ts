5d3a2ae2470c58d2edabe5a68e0f4ea1a004b644
Carlos Balarezo <balarezo_carlos@hotmail.com>
2018-02-15 23:01:14 -0500

    First Action Cool

--- TestMakerFreeWebApp/ClientApp/app/app.shared.module.ts
+++ TestMakerFreeWebApp/ClientApp/app/app.shared.module.ts
@@ -1,33 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { QuizListComponent } from './components/quiz/quiz-list.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+//import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+//import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        //CounterComponent,
+        //FetchDataComponent,
+        HomeComponent,
+        QuizListComponent,
+        QuizComponent,
+        AboutComponent,
+        LoginComponent,
+        PageNotFoundComponent
     ],
     imports: [
         CommonModule,
-        HttpModule,
+        HttpClientModule,
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: 'about', component: AboutComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'quiz/:id', component: QuizComponent },
+            //{ path: 'fetch-data', component: FetchDataComponent },
+            //{ path: '**', redirectTo: 'home' }
+            { path: '**', component : PageNotFoundComponent }
         ])
     ]
 })
---
+++ TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'quiz-list',
+    templateUrl: './quiz-list.component.html',
+    styleUrls: ['./quiz-list.component.css']
+})
+
+export class QuizListComponent  implements OnInit{
+    @Input() class: string;
+    title: string;
+    selectedQuiz: Quiz;
+    quizzes: Quiz[];
+
+    constructor(private http: HttpClient,
+                @Inject('BASE_URL') private baseUrl: string,
+                private router:Router) {
+        this.http = http;
+        this.baseUrl = baseUrl;
+    }
+
+    ngOnInit() {
+
+        console.log("QuizListComponent " +
+            " instantiated with the following class: "
+            + this.class);
+
+        //this.title = "Latest Quizzes";
+        var url = this.baseUrl + "api/quiz/";
+        debugger;
+        switch (this.class) {
+            case "latest":
+            default:
+                this.title = "Latest Quizzes";
+                url += "Latest/"
+                break;
+            case "byTitle":
+                this.title = "Quizzes by Title";
+                url += "ByTitle/";
+                break;
+            case "random":
+                this.title = "Random Quizzes";
+                url += "Random/";
+                break;
+        }
+
+        this.http.get<Quiz[]>(url).subscribe(result => {
+            this.quizzes = result;
+        }, error => console.error(error));
+
+    }
+
+    onSelect(quiz: Quiz) {
+        this.selectedQuiz = quiz;
+        console.log("quiz with ID" + this.selectedQuiz.Id + " has been selected");
+        this.router.navigate(["quiz", this.selectedQuiz.Id]);
+    }
+    
+
+}
